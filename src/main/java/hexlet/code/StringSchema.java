3d10c88be5cd243edea52f4e2d9d57971a2b8f9b
Vladislav.P <vladppk@protonmail.com>
2022-07-24 17:52:48 +0200

    upd

--- src/main/java/hexlet/code/StringSchema.java
+++ src/main/java/hexlet/code/StringSchema.java
@@ -1,11 +1,39 @@
 package hexlet.code;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StringSchema {
     Validator validator;
+    List<String> contains;
+    boolean required;
+    int minLength;
 
-    StringSchema(Validator validator){
-        this.validator = validator;
+    StringSchema(){
+        this.contains = new LinkedList<>();
+        this.required = false;
     }
 
 
+    public boolean isValid(String str){  // входной параметр соответствует всему остальному ю ноу
+        if (this.required && ((str.equals("")) || str == null)){
+            return false;
+        }
+
+
+        return false;
+    }
+
+    public void required() {    // required – любая непустая строка
+        this.required = true;
+    }
+
+    public boolean minLength(String str){  // minLength – строка равна или длиннее указанного числа
+
+        return false;
+    }
+
+    public void contains(String str){  //  contains – строка содержит определённую подстроку
+        this.contains.add(str);
+    }
 }
--- src/main/java/hexlet/code/Validator.java
+++ src/main/java/hexlet/code/Validator.java
@@ -1,13 +1,9 @@
 package hexlet.code;
 
-import javax.xml.validation.Schema;
-
 public class Validator {
-StringSchema schema;
-    Validator(){
-    }
 
-    public void string() {
-        this.schema = new StringSchema(this);
+
+    public StringSchema string() {
+        return new StringSchema();
     }
 }
