a84b04924e6c0dc9f32b0ce7ccf2826d29ea4e04
Vladislav.P <vladppk@protonmail.com>
2022-07-26 22:14:35 +0200

    add NumberSchema

---
+++ src/main/java/hexlet/code/NumberSchema.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public class NumberSchema {
+
+    // required – любое число включая ноль
+    // positive – положительное число
+    // range – диапазон, в который должны попадать числа включая границы
+
+}
