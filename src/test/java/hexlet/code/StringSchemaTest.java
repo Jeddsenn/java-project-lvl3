13fb309de8cbea196713a3ae25ce5f57b06a1f8c
Vladislav.P <vladppk@protonmail.com>
2022-08-02 16:27:02 +0200

    upd test

--- src/test/java/hexlet/code/StringSchemaTest.java
+++ src/test/java/hexlet/code/StringSchemaTest.java
@@ -16,15 +16,17 @@ class StringSchemaTest {
         assertTrue(schema.isValid(null)); // true
         schema.required();
 
+
         assertTrue(schema.isValid("what does the fox say")); // true
         assertTrue(schema.isValid("hexlet")); // true
         assertFalse(schema.isValid(null)); // false
+
         assertFalse(schema.isValid("")); // false
 
         assertTrue(schema.contains("wh").isValid("what does the fox say")); // true
-
         assertTrue(schema.contains("what").isValid("what does the fox say")); // true
         assertFalse(schema.contains("whatthe").isValid("what does the fox say")); // false
+
         // уже false, так как добавлена ещё одна проверка contains("whatthe")
     }
 
@@ -42,10 +44,8 @@ class StringSchemaTest {
         assertTrue(schema.contains("what").isValid("what does the fox say")); // true
         assertFalse(schema.contains("whatthe").isValid("what does the fox say")); // false
 
+        assertFalse(schema.minLength(100).isValid("what does the fox say"));
 
-
-        assertFalse(schema.minLength(0).isValid("what does the fox say"));
-        assertTrue(schema.minLength(1).isValid("what does the fox say")); // true
 
         assertFalse(schema.isValid("what does the fox say")); // false
 
