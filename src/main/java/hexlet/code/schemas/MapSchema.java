182f218a027a98df42273d8792cdcc7e2cde5d8d
Vladislav.P <vladppk@protonmail.com>
2022-08-03 12:45:05 +0200

    minimal isValid functionality

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,26 +1,63 @@
 package hexlet.code.schemas;
 
+
+import java.util.Map;
+
+
 public class MapSchema extends BaseSchema {
+    private int size;
+    private boolean sizeCheck;
+    private Map<String, BaseSchema> shape;
+    private boolean shapeCheck;
 
-
+    public MapSchema(){
+        super();
+        this.size = 0;
+        this.sizeCheck = false;
+        this.shapeCheck = false;
+    }
 
     @Override
-    public boolean isValid(Object map){
-        if (isRequired()) {
-            return !super.isValid(map);
+    public boolean isValid(Object map) {
+        if (!super.isValid(map)) {
+            if (isRequired()) {
+                return false;
+            }
         }
+
+        Map<String, Object> m = null;
+        if (map instanceof Map<?,?>){
+            m = (Map<String, Object>) map;
+        }
+
+        if (sizeCheck) {
+                if (m == null) {
+                    return false;
+                } else if (m.size() != this.size) {
+                    return false;
+                }
+            }
+
+
+
         return true;
     }
 
     public MapSchema required() {   //required – требуется тип данных Map
-        super.setRequired(true);
+        setRequired(true);
         return this;
     }
 
     public MapSchema sizeof(int size) {//sizeof – количество пар ключ-значений в объекте Map должно быть равно заданному
-
-
+        this.size = size;
+        this.sizeCheck = true;
         return this;
     }
 
+    // shape - позволяет описывать валидацию для значений объекта Map по ключам
+    public MapSchema shape(Map<String, BaseSchema> map) {
+        this.shape = map;
+        this.shapeCheck = true;
+        return this;
+    }
 }
