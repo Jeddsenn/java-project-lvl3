035687793b7d57970492811dc2da9a286c94622f
Jeddsenn <theecrapdump@gmail.com>
2022-09-10 18:58:32 +0200

    reworked MapSchema to use Predicate for validation

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,52 +1,13 @@
 package hexlet.code.schemas;
 
 import java.util.Map;
+import java.util.function.Predicate;
 
 public final class MapSchema extends BaseSchema {
-    private int size;
-    private boolean sizeCheck;
-    private Map<String, BaseSchema> shape;
-    private boolean shapeCheck;
 
-    public MapSchema() {
-        super();
-        this.size = 0;
-        this.sizeCheck = false;
-        this.shapeCheck = false;
+    public MapSchema(Class aClass) {
+        super(aClass);
     }
-
-    @Override
-    public boolean isValid(Object map) {
-        if (!super.isValid(map)) {
-            if (isRequired()) {
-                return false;
-            }
-        }
-
-        Map<String, BaseSchema> m = null;
-        if (map instanceof Map<?, ?>) {
-            m = (Map<String, BaseSchema>) map;
-        }
-
-        if (sizeCheck) {
-            if (m == null) {
-                return false;
-            } else if (m.size() != this.size) {
-                return false;
-            }
-        }
-
-        if (shapeCheck) {
-            for (String key : m.keySet()) {
-                BaseSchema sh = shape.get(key);
-                if (!sh.isValid(m.get(key))) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
 
     public MapSchema required() {   //required – требуется тип данных Map
         setRequired(true);
@@ -55,15 +16,23 @@ public final class MapSchema extends BaseSchema {
 
     //sizeof – количество пар ключ-значений в объекте Map должно быть равно заданному
     public MapSchema sizeof(int sz) {
-        this.size = sz;
-        this.sizeCheck = true;
+        Predicate<Map> func = x -> x.size() == sz;
+        addValidationToList(func);
         return this;
     }
 
     // shape - позволяет описывать валидацию для значений объекта Map по ключам
-    public MapSchema shape(Map<String, BaseSchema> map) {
-        this.shape = map;
-        this.shapeCheck = true;
+    public MapSchema shape(Map<String, BaseSchema> shape) {
+        Predicate<Map> func = x -> {
+            for (Object key : x.keySet()) {
+                BaseSchema sh = shape.get(key);
+                if (!sh.isValid(x.get(key))) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        addValidationToList(func);
         return this;
     }
 }
