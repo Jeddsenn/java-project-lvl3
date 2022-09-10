186e182561f2d28cbd2898c938d7f4449f19a458
Jeddsenn <theecrapdump@gmail.com>
2022-09-10 18:59:21 +0200

    reworked to use Predicate for validation

--- src/main/java/hexlet/code/schemas/NumberSchema.java
+++ src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,71 +1,29 @@
 package hexlet.code.schemas;
 
+
+import java.util.function.Predicate;
+
 public final class NumberSchema extends BaseSchema {
-    private boolean positive;
-    private int min;
-    private int max;
 
-    public int getMin() {
-        return min;
+    public NumberSchema(Class aClass) {
+        super(aClass);
     }
 
-    public int getMax() {
-        return max;
-    }
-
-    public NumberSchema() {
-        super();
-        this.positive = false;
-        this.max = Integer.MAX_VALUE;
-        this.min = Integer.MIN_VALUE;
-    }
-
-    @Override
-    public boolean isValid(Object num) {
-        if (isRequired()) {
-            if (!(num instanceof Number)) {
-                return false;
-            }
-        }
-        if (!super.isValid(num)) {
-            if (isRequired()) {
-                return false;
-            }
-        }
-
-        Integer castedNum = null;
-        if (num instanceof Number) {
-            castedNum = (Integer) num;
-        }
-
-        if (positive) {
-            if (castedNum == null) {
-                return true;
-            } else if (castedNum <= 0) {
-                return false;
-            }
-        }
-        if (getMin() != Integer.MIN_VALUE || getMax() != Integer.MAX_VALUE) {
-            if (castedNum < getMin() || castedNum > getMax()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public NumberSchema required() {    // required – любое число включая ноль
+    public NumberSchema required() {    // required – любое !число! включая ноль
         setRequired(true);
         return this;
     }
 
     public NumberSchema positive() {    // positive – положительное число
-        this.positive = true;
+        Predicate<Integer> func = x -> x > 0;
+        addValidationToList(func);
         return this;
     }
 
-    public NumberSchema range(int mi, int ma) { // range – диапазон, в который должны попадать числа включая границы
-        this.min = mi;
-        this.max = ma;
+    public NumberSchema range(int min, int max) { // range – диапазон, в который должны попадать числа включая границы
+        Predicate<Integer> func = x -> (x >= min && x <= max);
+        addValidationToList(func);
         return this;
     }
 }
+
--- src/main/java/hexlet/code/schemas/StringSchema.java
+++ src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,133 +1,31 @@
 package hexlet.code.schemas;
 
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.function.Predicate;
 
-public final class StringSchema extends BaseSchema {
-    private final List<String> contains;
-    private boolean minimalLengthCondition;
-    private int minLength;
+public class StringSchema extends BaseSchema {
 
-    public StringSchema() {
-        super();
-        this.contains = new LinkedList<>();
-        this.minLength = Integer.MIN_VALUE;
-        this.minimalLengthCondition = false;
+    public StringSchema(Class aClass) {
+        super(aClass);
     }
 
     @Override
-    public boolean isValid(Object str) {    // входной параметр соответствует активным условиям
-        String string = "";
-
-        if (!super.isValid(str)) {
-            if (isRequired()) {
-                return false;
-            }
-        } else if (Objects.equals(str, "")) {
-            if (isRequired()) {
-                return false;
-            }
-        }
-
-        if (str instanceof String) {
-            string = (String) str;
-        }
-
-
-        if (!this.contains.isEmpty()) {
-            for (String s : this.contains) {
-                if (!string.contains(s)) {
-                    return false;
-                }
-            }
-        }
-
-        if (minimalLengthCondition) {
-            return string.length() >= minLength;
-        }
-        return true;
-    }
-
-    public StringSchema contains(String str) {  //  contains – строка содержит определённую подстроку
-        this.contains.add(str);
-        return this;
-    }
-
-
-    public StringSchema required() {    // required – любая непустая строка
+    public final StringSchema required() {  // required – любая непустая строка
+        Predicate<String> func = x -> x.length() != 0;
+        addValidationToList(func);
         setRequired(true);
         return this;
     }
 
-    public StringSchema minLength(int ml) {  // minLength – строка равна или длиннее указанного числа
-        this.minLength = ml;
-        this.minimalLengthCondition = true;
+    public final StringSchema contains(String s) {  //  contains – строка содержит определённую подстроку
+        Predicate<String> func = x -> x.contains(s);
+        addValidationToList(func);
+        return this;
+    }
+
+    public final StringSchema minLength(int length) {  // minLength – строка равна или длиннее указанного числа
+        Predicate<String> func = x -> x.length() >= length;
+        addValidationToList(func);
         return this;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    boolean isValid = true;
-    boolean isNull = str == null;
-
-        if (isRequired()) {
-        isValid(str);
-    }
-        if (isRequired() && !isNull) {
-        if (str.equals("")) {
-            isValid = false;
-        }
-    }
-    String string = "";
-        if (str instanceof String){
-        string = (String) str;
-    }
-
-        if (!this.contains.isEmpty()) {
-        for (String s : this.contains) {
-            if (!string.contains(s)) {
-                isValid = false;
-                break;
-            }
-        }
-    }
-
-        if (minLength != -1) {
-        if (this.minLength > string.length()) {
-            return false;
-        }
-    }
-        return isValid;
-}*/
